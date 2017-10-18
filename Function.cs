efcb65b0a1acb59189cbb6da5f524a3b312a43df
Ryan Gribble <ryan.gribble@gmail.com>
2017-10-18 20:28:50 +1000

    Add project files

---
+++ Function.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace Octokit.Lambda.Demo
+{
+    public class Function
+    {
+        /// <summary>
+        /// Processes a GitHub WebHook
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            Console.WriteLine($"Webhook function executing");
+
+            try
+            {
+                var message = await ProcessWebHook(request);
+                Console.WriteLine($"SUCCESS: {message}");
+                return new APIGatewayProxyResponse
+                {
+                    Body = JsonConvert.SerializeObject(new Dictionary<string, string>() { { "message", message } }),
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                throw ex;
+            }
+        }
+
+        public async Task<string> ProcessWebHook(APIGatewayProxyRequest request)
+        {
+            // Configure GitHub Access
+            var token = Environment.GetEnvironmentVariable("github_token");
+            
+            var github = new GitHubClient(new ProductHeaderValue("octokit_lambda"))
+            {
+                Credentials = new Credentials(token)
+            };
+
+            Console.WriteLine($"Octokit connection initialized");
+
+            // Get webhook event type from header
+            var eventType = "unknown";
+            try
+            {
+                eventType = request.Headers["X-GitHub-Event"];
+            }
+            catch { }
+
+            // Deserialize webhook payload from body
+            dynamic data = null;
+            try
+            {
+                //Console.WriteLine($"Request Body: {request.Body ?? "null"}");
+                data = JsonConvert.DeserializeObject(request.Body);
+            }
+            catch { }
+
+            // Get webhook action from payload
+            var action = data?.action ?? "unknown";
+
+            Console.WriteLine($"Received GitHub WebHook event '{eventType}' action '{action}'");
+
+            // Process events
+            var message = "";
+            if (eventType == "issues" && action == "opened")
+            {
+                // Extract repo/issue details from request body
+                string owner = data?.repository?.owner?.login;
+                string repo = data?.repository?.name;
+                int issueNumber = data?.issue?.number ?? 0;
+
+                Console.WriteLine($"Processing {owner}/{repo}#{issueNumber}");
+
+                // Add "to_be_reviewed" label to the issue
+                var labelResponse = await github.Issue.Labels.AddToIssue(owner, repo, issueNumber, new[] { "to_be_reviewed" });
+
+                // Add a comment to the issue
+                var comment = "## :rotating_light: Review Pending :rotating_light:\nThankyou for your issue, someone will be taking a :eyes: shortly!";
+                var commentResponse = await github.Issue.Comment.Create(owner, repo, issueNumber, comment);
+
+                message = $"Issue {owner}/{repo}#{issueNumber} is now under review";
+            }
+            else
+            {
+                message = $"No processing required for event '{eventType}' action '{action}'";
+            }
+
+            return message;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Octokit.Lambbda.Demo
+
+This is a demonstration of a .NET Core lambda function that uses [Octokit.net]() to respond to a GitHub WebHook
+
+When receiving an `issues` `opened` webhook event, the lambda function will
+* Label the issue as `to_be_reviewed`
+* Comment on the issue
